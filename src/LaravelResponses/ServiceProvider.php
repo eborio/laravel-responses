9f53002f3d1d82e9691a21092c06d402445f3920
Eborio Linárez <eboriolinarez@gmail.com>
2025-03-13 08:41:31 -0400

    ServiceProvider

---
+++ src/LaravelResponses/ServiceProvider.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Eborio\LaravelResponses;
+
+class ServiceProvider extends \Illuminate\Support\ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+
+    }
+}
